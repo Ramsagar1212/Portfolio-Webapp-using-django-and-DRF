adf489d03277be039624644a2e1c64031cfff0b7
Ram Sagar Chaudhary <ramsagarchoudhary6@gmail.com>
2025-09-30 12:15:22 +0530

    create view only in reading mode

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Portfolio
+from .serializers import PortfolioSerializer
+# Create your views here
 
-# Create your views here.
+
+class PortfolioViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Portfolio.objects.all().order_by('-created_at')
+    serializer_class = PortfolioSerializer
+
