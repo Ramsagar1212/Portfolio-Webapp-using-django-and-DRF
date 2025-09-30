c3780c403d13da106995338e8929dc331fd4926e
Ram Sagar Chaudhary <ramsagarchoudhary6@gmail.com>
2025-09-30 12:04:50 +0530

    create models and serializers and register in admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Portfolio
 
 # Register your models here.
+
+# admin.site.register(Portfolio)
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('title','image','created_at')
--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length= 100)
+    description = models.TextField()
+    technologies = models.CharField(max_length=300, help_text='Comma seperated list of technologies used')
+    live_url = models.URLField(max_length=200, blank=True,null=True)
+    github_url = models.URLField(max_length=200, blank=True,null=True)
+    image = models.ImageField(upload_to='static/portfolio_images/',blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
